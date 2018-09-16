1303b48b683e166ed45c260403758f72e5692db5
Roberto Gutiérrez Sánchez <22207159+robertogs2@users.noreply.github.com>
2018-09-16 14:02:00 -0600

    Script for coches

---
+++ Project1/scriptCoche.py
@@ -0,0 +1,12 @@
+import random
+l = ["usado", "nuevo", "reparacion"]
+c = ["verde", "azul", "morado", "amarillo", "gris", "negro", "blanco", "café"]
+def sqlScript(amount):
+    for i in range(amount):
+        a = random.randint(0, 2)
+        es = l[a]
+        k = 0
+        if es != "nuevo":
+            k = random.randint(1, 400)
+        print("CALL AgregarCoche(" + str(random.randint(100000, 999999))+ ", " + str(random.randint(1, 20)) + ", " + c[random.randint(0, 7)] + ", " + es + ", " + str(k) + ", " + str(random.randint(1, 2))  + ");")
+sqlScript(50)
