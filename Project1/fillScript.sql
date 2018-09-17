5aed6663932c1cf2ea2fdf996ffe258e23ff04dc
Btrwrld <erickm968@gmail.com>
2018-09-16 22:36:59 -0600

    Added remaining data to the fillScript

--- Project1/fillScript.sql
+++ Project1/fillScript.sql
@@ -51,9 +51,12 @@ CALL AgregarUbicacion("100m Este, 100m Oeste de la bomba", 2);
 CALL AgregarUbicacion("Por el Palí", 3);
 CALL AgregarUbicacion("Por la finca de los Guzmán", 4);
 
+
+
 -- Agregar Concesionarios
 CALL AgregarConcesionario("Concesionario la UNO", 1);
 CALL AgregarConcesionario("Concesionario la DOS", 3);
+
 
 -- Agregar Talleres
 CALL AgregarTaller("Taller Alpha", 2, 1);
@@ -153,6 +156,117 @@ CALL AgregarCoche(244835, 20, "amarillo", "nuevo", 0, 1);
 CALL AgregarCoche(834463, 1, "azul", "reparacion", 164, 1);
 
 
+-- Agrega personas
+CALL AgregarPersona(123,"Celso", "Borges Mora", 22,88996633, 403, 1 );
+CALL AgregarPersona(147,"Andres", "Campos Castro", 69,79463521, 512, 2 );
+CALL AgregarPersona(159,"Teto", "Gutierrez Sánchez", 50,67924359, 256, 3 );
+CALL AgregarPersona(789,"Alejandro", "Calvo Porras", 18,87642025, 128, 4 );
+CALL AgregarPersona(456,"Iván", "Calvo Alverts", 33,79852630, 064, 4 );
+CALL AgregarPersona(741,"Allan", "Navarro Brenes", 40,78945612, 032, 1 );
+CALL AgregarPersona(321,"Crisptofer", "Fernández Fernández", 26,89764532, 016, 2 );
+CALL AgregarPersona(987,"Jimena", "Salas Alfaro", 22,69325896, 008, 3 );
+CALL AgregarPersona(951,"Alejandra", "Monterrey Arguedas", 23,74125896, 004, 4 );
+CALL AgregarPersona(333,"Erick", "Muñoz Alvarado", 20,70118002, 002, 1 );
+CALL AgregarPersona(205,"Raúl", "Madrigal Acuña", 26,70310065, 001, 2 );
+CALL AgregarPersona(258,"Ignacio", "Mora Jiménez", 28,86865320, 000, 3 );
+
+
+-- Agrega clientes
+CALL AgregarCliente(205);
+CALL AgregarCliente(258);
+
+-- Agrega mecánicos
+CALL AgregarMecanico("2008-12-10",500000,159,1,1);
+CALL AgregarMecanico("2010-1-15",800000,789,1,2);
+CALL AgregarMecanico("2016-2-20",700000,456,1,3);
+CALL AgregarMecanico("2017-3-03",200000,741,1,4);
+CALL AgregarMecanico("2015-4-22",900000,321,1,5);
+
+CALL AgregarMecanico("2018-5-27",800000,987,2,6);
+CALL AgregarMecanico("2015-6-29",500000,951,2,7);
+CALL AgregarMecanico("2017-7-24",600000,333,2,8);
+CALL AgregarMecanico("2014-8-15",600000,123,2,9);
+CALL AgregarMecanico("2015-9-17",600000,147,2,10);
+
+
+-- Agrega reparaciones
+CALL AgregarReparacion(1, "Nacho chocó saliendo de la concesionaria");
+CALL AgregarReparacion(2, "Pancho chocó saliendo de la universidad");
+CALL AgregarReparacion(3, "Andrés chocó saliendo de la chicharronera");
+CALL AgregarReparacion(4, "Crisptofer chocó saliendo de la casa");
+CALL AgregarReparacion(5, "Raúl chocó saliendo de la universidad");
+CALL AgregarReparacion(6, "Erick chocó saliendo de la chicharronera");
+CALL AgregarReparacion(7, "Alejandra chocó saliendo de la concesionaria");
+CALL AgregarReparacion(8, "Alejandro chocó saliendo de la casa");
+CALL AgregarReparacion(9, "Iván chocó saliendo de la universidad");
+CALL AgregarReparacion(10, "Celso chocó saliendo de la chicharronera");
+CALL AgregarReparacion(11, "Teto chocó saliendo de la universidad");
+CALL AgregarReparacion(12, "Pablito chocó un carrito");
+CALL AgregarReparacion(13, "Pedro chocó saliendo de la universidad");
+CALL AgregarReparacion(14, "Susana chocó saliendo de la casa");
+CALL AgregarReparacion(15, "Ingrid chocó saliendo de la chicharronera");
+CALL AgregarReparacion(16, "Adrián chocó saliendo de la casa");
+CALL AgregarReparacion(17, "Mariana chocó saliendo de la concesionaria");
+CALL AgregarReparacion(18, "Tiffany chocó saliendo de la casa");
+CALL AgregarReparacion(19, "Cristina chocó saliendo de la chicharronera");
+CALL AgregarReparacion(20, "Carolina chocó saliendo de la casa");
+CALL AgregarReparacion(21, "Aymarú chocó saliendo de la concesionaria");
+CALL AgregarReparacion(22, "Fernanda chocó saliendo de la chicharronera");
+CALL AgregarReparacion(23, "Jhousen chocó saliendo de la casa");
+CALL AgregarReparacion(24, "Jose chocó saliendo de la concesionaria");
+CALL AgregarReparacion(25, "Bryan chocó saliendo de la casa");
+CALL AgregarReparacion(26, "Chaco chocó saliendo de la concesionaria");
+CALL AgregarReparacion(27, "Juan chocó saliendo de la universidad");
+CALL AgregarReparacion(28, "Sebastián chocó saliendo de la chicharronera");
+CALL AgregarReparacion(29, "Allan chocó saliendo de la casa");
+CALL AgregarReparacion(30, "Pablo chocó saliendo de la universidad");
+CALL AgregarReparacion(31, "Ángel chocó saliendo de la chicharronera");
+CALL AgregarReparacion(32, "Daniel chocó saliendo de la concesionaria");
+CALL AgregarReparacion(33, "Melissa chocó saliendo de la casa");
+CALL AgregarReparacion(34, "Óscar chocó saliendo de la universidad");
+CALL AgregarReparacion(35, "Kenneth chocó saliendo de la chicharronera");
+CALL AgregarReparacion(36, "Karen chocó saliendo de la universidad");
+CALL AgregarReparacion(37, "María chocó un carrito");
+CALL AgregarReparacion(38, "Mauricio chocó saliendo de la universidad");
+CALL AgregarReparacion(39, "Milton chocó saliendo de la casa");
+CALL AgregarReparacion(40, "Jeff chocó saliendo de la chicharronera");
+CALL AgregarReparacion(41, "Carlos chocó saliendo de la casa");
+CALL AgregarReparacion(42, "Belinda chocó saliendo de la concesionaria");
+CALL AgregarReparacion(43, "Gabriela chocó saliendo de la casa");
+CALL AgregarReparacion(44, "Gregory chocó saliendo de la chicharronera");
+CALL AgregarReparacion(45, "Jorman chocó saliendo de la casa");
+CALL AgregarReparacion(46, "Saúl chocó saliendo de la concesionaria");
+CALL AgregarReparacion(47, "Fernando chocó saliendo de la chicharronera");
+CALL AgregarReparacion(48, "Érika chocó saliendo de la casa");
+CALL AgregarReparacion(49, "Alexander chocó saliendo de la concesionaria");
+CALL AgregarReparacion(50, "Chino chocó saliendo de la casa");
+
+-- Agrega ventas
+CALL AgregarCompra("2018-12-01",30000000,1,1,1);
+CALL AgregarCompra("2016-11-02",40000000,2,1,2);
+CALL AgregarCompra("2017-10-03",50000000,1,1,3);
+CALL AgregarCompra("2015-9-04",60000000,2,1,4);
+CALL AgregarCompra("2014-8-05",70000000,1,1,5);
+CALL AgregarCompra("2013-7-06",80000000,2,1,6);
+CALL AgregarCompra("2012-4-07",90000000,1,1,7);
+CALL AgregarCompra("2011-5-08",100000000,2,1,8);
+CALL AgregarCompra("2010-6-09",110000000,1,1,9);
+CALL AgregarCompra("2009-2-10",120000000,2,1,10);
+CALL AgregarCompra("2008-3-11",130000000,1,1,11);
+CALL AgregarCompra("2009-1-12",25000000,2,1,12);
+CALL AgregarCompra("2010-12-13",20000000,1,1,13);
+CALL AgregarCompra("2011-11-14",15000000,2,1,14);
+CALL AgregarCompra("2012-10-15",1000000,1,2,15);
+CALL AgregarCompra("2013-9-16",2000000,2,2,16);
+CALL AgregarCompra("2014-8-17",1000000,1,2,17);
+CALL AgregarCompra("2015-7-18",9000000,2,2,18);
+CALL AgregarCompra("2016-6-19",8000000,1,2,19);
+CALL AgregarCompra("2017-5-20",7000000,2,2,20);
+CALL AgregarCompra("2018-4-21",6000000,1,2,21);
+CALL AgregarCompra("2007-3-22",5000000,2,2,22);
+CALL AgregarCompra("2004-2-23",4000000,1,2,23);
+CALL AgregarCompra("2003-1-24",3000000,2,2,24);
+CALL AgregarCompra("2002-12-25",2000000,1,2,25);
 
 
 CALL ObtenerDireccionCompleta(2);
