ff06ac02977997fba88847e25b82483820747373
Roberto Gutiérrez Sánchez <22207159+robertogs2@users.noreply.github.com>
2018-09-25 23:48:48 -0600

    Merge branch 'master' of https://github.com/robertogs2/bases

--- Project1/GUI/src/main/Main.java
+++ Project1/GUI/src/main/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import stages.preview.previewController;
 
