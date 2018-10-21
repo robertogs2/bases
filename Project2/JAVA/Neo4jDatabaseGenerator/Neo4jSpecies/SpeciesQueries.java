9aeb1f0de9bd8428b01c6f90ab39f93882d0b736
Roberto Gutiérrez Sánchez <22207159+robertogs2@users.noreply.github.com>
2018-10-20 23:42:28 -0600

    Queries almost done

--- Project2/JAVA/Neo4jDatabaseGenerator/Neo4jSpecies/SpeciesQueries.java
+++ Project2/JAVA/Neo4jDatabaseGenerator/Neo4jSpecies/SpeciesQueries.java
@@ -1,22 +1,18 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Result;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.io.fs.FileUtils;
-import java.util.Scanner; 
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import org.neo4j.graphdb.*;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
 public class SpeciesQueries {
 
     private static final File databaseDirectory = new File("databases/species");
     private GraphDatabaseService graphDb;
     private Scanner scan;
 
-    public static void main(final String[] args) throws IOException {
+    public static void main(final String[] args) throws IOException, InterruptedException {
         SpeciesQueries app = new SpeciesQueries();
         app.loadDatabase();
         app.listen();
@@ -24,40 +20,61 @@ public class SpeciesQueries {
     }
 
     private void loadDatabase() throws IOException {
-    	FileUtils.deleteRecursively( databaseDirectory );
         scan = new Scanner(System.in);
         graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(databaseDirectory);
         registerShutdownHook(graphDb);
     }
 
-    private void listen() {
+    private void listen() throws IOException, InterruptedException {
         String input;
-        while(true){
+        while (true) {
             System.out.println("Please enter a query");
-            input = scan.next();
-            if(input.toLowerCase().equals("exit") || input.toLowerCase().equals("e")){
+            input = scan.nextLine();
+            //System.out.println(input);
+            if (input.toLowerCase().equals("exit")) {
                 break;
             }
-            else{
+            else if(input.toLowerCase().equals("clear")){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            }
+            else {
                 result(input);
             }
         }
-        
+
         System.out.println("Finished the run");
     }
 
-    private void result(String input){
-        try (Transaction ignored = graphDb.beginTx(); Result result = graphDb.execute(input)){
-            while ( result.hasNext() ){
-                    Map<String,Object> row = result.next();
-                    String line;
-                    for ( Entry<String,Object> column : row.entrySet() ){
-                        line = column.getKey() + ": " + column.getValue() + "; ";
-                        System.out.println(line);
+    private void result(String input) {
+        System.out.println("Executing: " + input);
+        try (Transaction ignored = graphDb.beginTx(); Result result = graphDb.execute(input)) {
+            while (result.hasNext()) {
+                Map<String, Object> res = result.next();
+                for (Map.Entry<String, Object> entry: res.entrySet()) {
+                    Entity entity = (Entity)entry.getValue();
+                    try{
+                        Node node = (Node)entity;
+                        node.getRelationships();
+                        System.out.print("Found a node as " + entry.getKey() + ", with \n\tLabels: ");
+                        System.out.println();
+                        for(Label label : node.getLabels()){
+                            System.out.println("\t\t" + label.name());
+                        }
                     }
+                    catch (Exception e){
+                        e.printStackTrace();
+                        Relationship relationship = (Relationship)entity;
+                        System.out.print("Found a relationship with type: " + relationship.getType());
+                    }
+                    System.out.println("\n\tProperties: ");
+                    for(String k : entity.getPropertyKeys()){
+                        System.out.print("\t\t" + k + " : " + entity.getProperty(k) + " ");
+                        System.out.println();
+                    }
+                    System.out.println();
+                }
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -68,12 +85,12 @@ public class SpeciesQueries {
         graphDb.shutdown();
     }
 
-    private static void registerShutdownHook( final GraphDatabaseService graphDb ){
-        Runtime.getRuntime().addShutdownHook( new Thread(){
+    private static void registerShutdownHook(final GraphDatabaseService graphDb) {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
             @Override
-            public void run(){
+            public void run() {
                 graphDb.shutdown();
             }
-        } );
+        });
     }
 }
--- Project2/JAVA/Neo4jDatabaseGenerator/Neo4jSpecies/data/importCSV.cypher
+++ Project2/JAVA/Neo4jDatabaseGenerator/Neo4jSpecies/data/importCSV.cypher
@@ -5,10 +5,10 @@ DETACH DELETE n;
 USING PERIODIC COMMIT
 LOAD CSV WITH HEADERS FROM "file:///%s" AS row
 FOREACH(ignoreMe IN CASE WHEN toInt(row.IDType) = 1 THEN [1] ELSE [] END | 
-    CREATE (:Species:Animal {NonScientificName: row.NonScientificName, ScientificName: row.ScientificName, IDSpecies : row.IDSpecies})
+    CREATE (:Species:Animal {NonScientificName: row.NonScientificName, ScientificName: row.ScientificName, IDSpecies : toInt(row.IDSpecies)})
 )
 FOREACH(ignoreMe IN CASE WHEN NOT toInt(row.IDType) = 1 THEN [1] ELSE [] END | 
-    CREATE (:Species:Vegetal {NonScientificName: row.NonScientificName, ScientificName: row.ScientificName, IDSpecies : row.IDSpecies})
+    CREATE (:Species:Vegetal {NonScientificName: row.NonScientificName, ScientificName: row.ScientificName, IDSpecies : toInt(row.IDSpecies)})
 );
 
 //Create index to ensure quick lookup when creating relations
@@ -18,8 +18,8 @@ CREATE INDEX ON :Vegetal(IDSpecies);
 // Creating chain relations
 USING PERIODIC COMMIT
 LOAD CSV WITH HEADERS FROM "file:///%s" AS row
-MATCH (speciesA:Species {IDSpecies: row.IDEater})
-MATCH (speciesB:Species {IDSpecies: row.IDFood})
+MATCH (speciesA:Species {IDSpecies: toInt(row.IDEater)})
+MATCH (speciesB:Species {IDSpecies: toInt(row.IDFood)})
 MERGE (speciesA)-[:Eats]->(speciesB);
 
 MATCH (n)
