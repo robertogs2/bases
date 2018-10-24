d1a8e0e9294527e3f3a84757fb51f47fde21c148
Roberto Gutiérrez Sánchez <22207159+robertogs2@users.noreply.github.com>
2018-10-23 21:44:45 -0700

    ips

--- Project2/DB_Scripts/SPs/SpeciesProcedures.sql
+++ Project2/DB_Scripts/SPs/SpeciesProcedures.sql
@@ -1,5 +1,5 @@
 -- DBCC CHECKIDENT ('[Species]', RESEED, 0)
-
+DROP PROCEDURE GetBeings
 CREATE PROCEDURE GetBeings AS
 	WITH IDTable 
 	AS (SELECT fk_idConsumer AS id FROM FoodChainLink
@@ -7,8 +7,10 @@ CREATE PROCEDURE GetBeings AS
 	SELECT fk_idFood AS id FROM FoodChainLink)
 	SELECT IDSpecies, fk_idSpeciesType AS IDType, ScientificName, NonScientificName FROM Species
 	INNER JOIN IDTable ON IDTable.id = Species.idSpecies
+	WHERE fk_idSpeciesType = 1 OR fk_idSpeciesType = 2
 GO
 
+DROP PROCEDURE GetChain
 CREATE PROCEDURE GetChain AS
 	SELECT fk_idConsumer AS IDConsumer, fk_idFood AS IDFood
 	FROM FoodChainLink
