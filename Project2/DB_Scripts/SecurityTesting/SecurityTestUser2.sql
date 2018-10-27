e2a051cbbda6c898f9ecb517e4b3ca893efd8c7e
campos-97 <joseacampos97@gmail.com>
2018-10-26 18:08:43 -0600

    Security
    Scripts for security testing

---
+++ Project2/DB_Scripts/SecurityTesting/SecurityTestUser2.sql
@@ -0,0 +1,62 @@
+
+-- User2 permissions were set for BASESTECBAK data base
+USE BASESTEC; 
+
+--				____________________________
+--_____________/Allowed operations for User2\___________
+
+-- Recover data queries
+SELECT * FROM Employee;
+
+SELECT * FROM Country;
+
+-- Store data queries
+INSERT INTO Characteristic
+("Name", "Value")
+VALUES
+('Test_User2_Insert', 'Test_User2_Insert');
+
+INSERT INTO City
+("Name", fk_idState)
+VALUES
+('Test_User2_Insert', 1);
+
+-- Reverse changes to return DB to its original state
+
+DELETE FROM Characteristic
+WHERE Characteristic.Name = 'Test_User2_Insert';
+
+DELETE FROM City
+WHERE City.Name = 'Test_User2_Insert';
+
+-- Store Procedures
+EXEC GetBeings;
+
+--				______________________________
+--_____________/Forbidden operations for User2\___________
+
+-- Recover data queries
+SELECT * FROM Park;
+SELECT * FROM Person;
+
+-- Store data queries
+INSERT INTO AssignedArea
+("Name")
+VALUES
+('Test_User2_Insert');
+
+INSERT INTO Speciality
+("Name")
+VALUES
+('Test_User2_Insert');
+
+-- Reverse changes to return DB to its original state
+
+DELETE FROM AssignedArea
+WHERE AssignedArea.Name = 'Test_User2_Insert';
+
+DELETE FROM Speciality
+WHERE Speciality.Name = 'Test_User2_Insert';
+
+-- Store Procedures
+EXEC GetChain;
