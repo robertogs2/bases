fd0bd4c8721db8ec87d21a67a571bc09546a01de
Roberto Gutiérrez Sánchez <22207159+robertogs2@users.noreply.github.com>
2018-10-27 01:12:20 -0700

    Concurrency organization

---
+++ Project2/DB_Scripts/7-Concurrency/cursorLockDemonstration2.sql
@@ -0,0 +1,10 @@
+USE BASESTEC;
+GO
+
+SELECT *--Tour.Price
+FROM Tour 
+-- If we use an id that the curson has already visited, 
+-- we wont be able to access its data since its blocked,
+-- if we use an id it hast visited we can see its data normally
+--WHERE Tour.idTour = 2
+ORDER BY(Tour.idTour) DESC
---
+++ Project2/DB_Scripts/7-Concurrency/isolationLevelsDemonstration.sql
@@ -0,0 +1,42 @@
+-- Set to READ UNCOMMITTED allows us to read changes that have not been commited
+-- it also help us to avoid locks that would be produced by other transactions modifyinG the data
+--SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+
+-- Set to READ COMMITTED prevent us from reading changes that have not been commited
+-- prevents dirty reads, the query is frozen until the transaction is commited
+--SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+-- Set to REPEATABLE READ prevent us from reading changes that have not been commited
+-- and from modifying data that is being read. This helps us to avoid changing data that some
+-- other transaction is reading. Allows insertion and read at the same time
+--SET TRANSACTION ISOLATION LEVEL REPEATABLE READ
+
+-- Set to SERIALIZABLE prevent us from reading changes that have not been commited,
+-- from modifying data that is being readand from inserting data in a table that is being read. 
+-- This locks us away from modifying data that is being used
+--SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+
+-- This shoud be blocked whe updating in all levels but 
+-- the read uncommited
+
+SELECT Park.Name 
+FROM Park
+WHERE Park.idPark = 1
+
+
+--Inserts are allowed
+/*
+INSERT INTO Park
+	(Name,foundationDate)
+VALUES
+	('Hoenh','February 26, 19')
+*/
+
+--Updates lock till the reading ends
+/*
+UPDATE Park
+SET Park.Name = 'Jurassic park'
+WHERE Park.idPark = 3 
+*/
+
+
